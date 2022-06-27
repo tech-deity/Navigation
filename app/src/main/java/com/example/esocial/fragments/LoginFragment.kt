870007f1376b0110af3280ba5aefef91ca0bb0e6
Vishal <31177415+tech-deity@users.noreply.github.com>
2022-06-27 16:27:24 +0530

    2 Commit

--- app/src/main/java/com/example/esocial/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/esocial/fragments/LoginFragment.kt
@@ -5,16 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
 import com.example.esocial.R
 
 class LoginFragment : Fragment() {
 
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        val view= inflater.inflate(R.layout.fragment_login, container, false)
+        val movetoRegister : TextView = view.findViewById(R.id.register_tv)
+        movetoRegister.setOnClickListener{
+            findNavController().navigate(R.id.action_loginFragment_to_register)
+        }
+
+        return view
     }
 
 
